0d8fee28860edbb81a0241012b8258eaf3d69baa
henrikaly <henrikaly@gmail.com>
2017-02-28 10:09:00 +0100

    Added a new func

--- my.py
+++ my.py
@@ -1,6 +1,13 @@
-def cal(a,b):
-a=2;
-b=2
-c=a+b;
 
-print c
+def cal(a, b):
+    # type: (int, int) -> int
+
+    """
+
+    :rtype: This func should return an int
+    """
+    c = a + b;
+
+    return c
+
+print cal(2, 8)
